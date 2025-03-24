00b1b683091e9eaccc8e29cf85b9cef654ff9a65
Yngvarr <k.yngvarr@gmail.com>
2025-03-24 16:50:47 +0100

    credits

---
+++ src/credits_window.gd
@@ -0,0 +1,43 @@
+@tool
+extends Window
+
+
+const CREDITS := """
+[center][wave amp=50.0 freq=5.0 connected=1]%s[/wave][/center]
+
+Brought to you by:
+[ul bullet=‣]
+Yngvarr — code, sound, game design;
+Syudzius — art, game design.
+[/ul]
+
+With use of amazing third-party assets:
+[ul bullet=‣]
+[url=https://plus.excalidraw.com/excalifont]Excalifont[/url] by Excalidraw team;
+[/ul]
+
+And these wonderful tools:
+[ul bullet=‣]
+[url=https://godotengine.org/]Godot Engine[/url] by Godot contributors.
+[url=https://www.bitwig.com/]Bitwig Studio[/url] by Bitwig GmbH.
+[url=https://www.aseprite.org/]Aseprite[/url] by Igara Studio S.A.
+[/ul]
+
+Special thanks to all my homies! 🐺
+"""
+
+
+@export_group("Internal")
+@export var label: RichTextLabel
+
+
+func _ready() -> void:
+    label.text = CREDITS % ProjectSettings.get_setting('application/config/name')
+
+    if Engine.is_editor_hint():
+        close_requested.connect(hide)
+        label.meta_clicked.connect(on_meta_clicked)
+
+
+func on_meta_clicked(meta: Variant) -> void:
+    OS.shell_open(str(meta))
--- src/main_menu.gd
+++ src/main_menu.gd
@@ -8,15 +8,10 @@ extends Node2D
 		update_configuration_warnings()
 
 @export_group("Internal")
-@export var quit_button: Button:
-	set(value):
-		quit_button = value
-		update_configuration_warnings()
-
-@export var start_button: Button:
-	set(value):
-		start_button = value
-		update_configuration_warnings()
+@export var quit_button: Button
+@export var start_button: Button
+@export var credits_button: Button
+@export var credits_window: Window
 
 
 func _ready() -> void:
@@ -25,6 +20,7 @@ func _ready() -> void:
 			quit_button.visible = false
 
 		start_button.pressed.connect(start_game)
+		credits_button.pressed.connect(open_credits)
 		quit_button.pressed.connect(quit_game)
 
 
@@ -34,20 +30,15 @@ func _get_configuration_warnings() -> PackedStringArray:
 	if ProjectSettings.get_setting('application/config/name') == 'Game Template':
 		warns.push_back("Please, change the game's name.")
 
-	if not quit_button:
-		warns.push_back("Quit button must not be null.")
-
-	if not start_button:
-		warns.push_back("Start button must not be null.")
-
-	if not game_scene_name:
-		warns.push_back("Game scene name must be set.")
-
 	return warns
 
 
 func start_game() -> void:
 	Global.change_scene(game_scene_name)
+
+
+func open_credits() -> void:
+	credits_window.show()
 
 
 func quit_game() -> void:

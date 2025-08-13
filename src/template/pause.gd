2bd53b0ced50913b56012a9e3a280b7210ba16b0
Yngvarr <k.yngvarr@gmail.com>
2025-08-13 20:10:36 +0200

    return focus after pause

--- src/template/pause.gd
+++ src/template/pause.gd
@@ -4,13 +4,12 @@ extends Node
 ## Must have [code]process_mode == PROCESS_MODE_WHEN_PAUSED[/code].
 @export var pause_screen: PauseMenu
 
-
 var to_drop_next := false
+var last_focused: Control
 
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed(&"game_pause"):
-
 		if get_tree().paused:
 			unpause(false)
 		else:
@@ -18,6 +17,11 @@ func _process(_delta: float) -> void:
 
 
 func pause() -> void:
+	if to_drop_next:
+		to_drop_next = false
+		return
+
+	last_focused = get_viewport().gui_get_focus_owner()
 	to_drop_next = false
 	get_tree().paused = true
 	pause_screen.pause()
@@ -30,6 +34,9 @@ func unpause(ignore_drop := true) -> void:
 
 	get_tree().paused = false
 	pause_screen.unpause()
+
+	if last_focused:
+		last_focused.grab_focus()
 
 
 func drop_next() -> void:

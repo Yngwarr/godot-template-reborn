d87b1ab26ccb633f244332436ab3c17371a5bdba
Yngvarr <k.yngvarr@gmail.com>
2025-11-16 16:11:50 +0100

    add an util to calculate random pitch

--- src/utils/misc_utils.gd
+++ src/utils/misc_utils.gd
@@ -4,3 +4,13 @@ extends Object
 
 static func get_focus_owner(this: Node) -> Control:
 	return this.get_viewport().gui_get_focus_owner()
+
+
+static func eerp(a: float, b: float, t: float) -> float:
+	return a * exp(t * log(b / a))
+
+
+static func random_pitch(base: float, offset := .1) -> float:
+	assert(offset <= base)
+
+	return eerp(base - offset, base + offset, randf())
